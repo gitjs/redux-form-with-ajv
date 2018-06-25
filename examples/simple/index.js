a82223f424114b523b07f3fa01b3a5dff33b0a83
Jan Salloth <jan.salloth@gmail.com>
2018-06-25 17:13:35 +0200

    removed hot

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-import { reducer as reduxFormReducer } from 'redux-form';
-import { Code, Markdown, Values } from 'redux-form-website-template';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers } from "redux";
+import { reducer as reduxFormReducer } from "redux-form";
+import { Code, Markdown, Values } from "redux-form-website-template";
 
-const content = document.createElement('div');
-content.setAttribute('id', 'content');
+const content = document.createElement("div");
+content.setAttribute("id", "content");
 const dest = document.body.appendChild(content);
 const reducer = combineReducers({
   form: reduxFormReducer // mounted under "form"
 });
-const store = (window.devToolsExtension ? window.devToolsExtension()(createStore) : createStore)(reducer);
+const store = (window.devToolsExtension
+  ? window.devToolsExtension()(createStore)
+  : createStore)(reducer);
 
 const showResults = values =>
   new Promise(resolve => {
@@ -23,11 +25,11 @@ const showResults = values =>
   });
 
 let render = () => {
-  const Form = require('./Form').default;
-  const raw = require('!!raw-loader!./Form');
+  const Form = require("./Form").default;
+  const raw = require("!!raw-loader!./Form");
   ReactDOM.render(
     <Provider store={store}>
-      <div style={{ margin: '0 200px' }}>
+      <div style={{ margin: "0 200px" }}>
         <h2>Form</h2>
         <Form onSubmit={showResults} />
         <Values form="syncValidation" />
@@ -39,27 +41,5 @@ let render = () => {
     dest
   );
 };
-
-if (module.hot) {
-  // Support hot reloading of components
-  // and display an overlay for runtime errors
-  const renderApp = render;
-  const renderError = error => {
-    const RedBox = require('redbox-react');
-    ReactDOM.render(<RedBox error={error} className="redbox" />, dest);
-  };
-  render = () => {
-    try {
-      renderApp();
-    } catch (error) {
-      renderError(error);
-    }
-  };
-  const rerender = () => {
-    setTimeout(render);
-  };
-  module.hot.accept('./Form', rerender);
-  module.hot.accept('!!raw-loader!./Form', rerender);
-}
 
 render();
